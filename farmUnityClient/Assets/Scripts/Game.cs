9ca7bb1292b5ca048cab91c26690d5ef6b0c2c87
pavel.sinitski <pavel.sinitski@bpmobile.com>
2017-12-18 15:21:34 +0300

    Unity - executing js scripts.

--- farmUnityClient/Assets/Scripts/Game.cs
+++ farmUnityClient/Assets/Scripts/Game.cs
@@ -1,16 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.IO;
 using UnityEngine;
 
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        var engine = new Jurassic.ScriptEngine();
+        engine.Evaluate(ReadScript("Resources/api.js"));
+        engine.Evaluate(ReadScript("Resources/item.js"));
+        engine.Evaluate(ReadScript("Resources/staticData.js"));
+        engine.Evaluate(ReadScript("Resources/shop.js"));
+        engine.Evaluate(ReadScript("Resources/bag.js"));
+        engine.Evaluate(ReadScript("Resources/farm.js"));
+        string initialData = engine.CallGlobalFunction<string>("getDataAsString");
+        Debug.Log(initialData);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private string ReadScript(string path)
+    {
+        StreamReader reader = new StreamReader(path);
+        string scriptAsString = reader.ReadToEnd();
+        reader.Close();
+        return scriptAsString;
+    }
 }
